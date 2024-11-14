3198d1f1a4a1287bd2664f6cd1563761f0f50995
USER <USER@LUCAS-2008>
2024-11-14 17:02:24 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppEventos/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppEventos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
